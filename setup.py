95b1ae0f64f02056d5b14345ef63a2c734472d80
Maurits van Rees <maurits@vanrees.org>
2012-09-28 17:13:17 +0200

    Renamed CHANGES.txt to CHANGES.rst

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ long_description = (
     'Change history\n'
     '**************\n'
     + '\n' +
-    read('CHANGES.txt')
+    read('CHANGES.rst')
     + '\n' +
     'Detailed Documentation\n'
     '**********************\n'
