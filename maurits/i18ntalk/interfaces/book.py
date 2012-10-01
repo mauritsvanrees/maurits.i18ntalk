f78a60a7ef2e74ba199b8ced2a4e6142b57b7f89
Maurits van Rees <maurits@vanrees.org>
2012-10-01 13:58:10 +0200

    An IBook marker interface is good enough.

--- maurits/i18ntalk/interfaces/book.py
+++ maurits/i18ntalk/interfaces/book.py
@@ -1,24 +1,5 @@
 from zope.interface import Interface
-# -*- Additional Imports Here -*-
-from zope import schema
-
-from maurits.i18ntalk import i18ntalkMessageFactory as _
-
 
 
 class IBook(Interface):
     """Information about a book"""
-
-    # -*- schema definition goes here -*-
-    author = schema.TextLine(
-        title=_(u"Author"),
-        required=True,
-        description=_(u"Author of this book"),
-    )
-#
-    stars = schema.Int(
-        title=_(u"Stars"),
-        required=False,
-        description=_(u"How well do you like this book? Use number 1 through 5."),
-    )
-#
