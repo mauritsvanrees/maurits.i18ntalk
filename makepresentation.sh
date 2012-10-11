31a21d24e7e8693ae172af6c79605803d1be2566
Maurits van Rees <maurits@vanrees.org>
2012-10-11 13:59:03 +0200

    Fix link in makepresentation.sh

--- makepresentation.sh
+++ makepresentation.sh
@@ -2,5 +2,5 @@
 infile="talk.rst"
 rst2html="rst2html.py"
 css="static/medium.css"
-github_image=".. image:: https://raw.github.com/mauritsvanrees/talks/master/"
+github_image=".. image:: https://raw.github.com/mauritsvanrees/maurits.i18ntalk/master/"
 cat  $infile | sed "s|$github_image|.. image:: |" | $rst2html --link-stylesheet --stylesheet-path=static/all.css,$css | sed "s/div class=\"section\"/div class=\"section\" tabindex=\"1\"/g" > presentation.html
